3b8dd3cf01a3b2f936a3c04b3027eca1eda3d346
dannguyen <dansonguyen@gmail.com>
2014-02-27 21:34:28 -0500

    view and model stuff

--- app/models/congressmember.rb
+++ app/models/congressmember.rb
@@ -1,2 +1,12 @@
 class Congressmember < ActiveRecord::Base
+
+
+  def full_name
+    first_name + ' ' + last_name
+  end
+
+  def image_url
+    "http://placekitten.com/#{100 + rand(20)}/#{150 + rand(20)}"
+  end
+
 end
